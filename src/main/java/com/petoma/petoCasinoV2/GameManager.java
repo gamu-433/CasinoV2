19e2e4c734bb94ead127fb6a0103ab4ed731537f
Somen1000 <147126233+Somen1000@users.noreply.github.com>
2025-01-12 18:43:15 +0900

    主に演出面での改良

--- src/main/java/com/petoma/petoCasinoV2/GameManager.java
+++ src/main/java/com/petoma/petoCasinoV2/GameManager.java
@@ -2,6 +2,7 @@ package com.petoma.petoCasinoV2;
 
 import jp.jyn.jecon.Jecon;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
@@ -10,6 +11,7 @@ import java.math.BigDecimal;
 import java.util.*;
 
 public class GameManager {
+    private List<String> winners = new ArrayList<>();
 
     private Jecon jecon;
     private final Map<Player, Integer> money = new HashMap<>();
@@ -60,6 +62,13 @@ public class GameManager {
 
         startCount = 75;
         startCount();
+
+        String json = "[{\"text\":\" 以下の選択肢をクリックすることで参加します！\\n\\n\",\"color\":\"green\",\"bold\":true},{\"text\":\"＜\",\"color\":\"white\",\"bold\":true},{\"text\":\"小にかける!\",\"color\":\"red\",\"bold\":true,\"hoverEvent\":{\"action\":\"show_text\",\"value\":[{\"text\":\"小(サイコロの和 2~6)にかけます\\n当選すると掛け金の2倍の額をゲット！\",\"color\":\"aqua\"}]}," +
+                "\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/chn low\"}},{\"text\":\"＞　＜\",\"color\":\"white\",\"bold\":true,\"hoverEvent\":{\"action\":\"show_text\",\"value\":[{\"text\":\"\",\"color\":\"gold\"}]}},{\"text\":\"ぴったりセブン!\",\"color\":\"aqua\",\"bold\":true,\"hoverEvent\":{\"action\":\"show_text\",\"value\":[{\"text\":\"ぴったセブン(サイコロの和 7) にかける\\n当選すると掛け金の 3倍! の額をゲット！\",\"color\":\"aqua\"}]}," +
+                "\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/chn lucky7\"}},{\"text\":\"＞　＜\",\"color\":\"white\",\"bold\":true,\"hoverEvent\":{\"action\":\"show_text\",\"value\":[{\"text\":\"\",\"color\":\"aqua\"}]}},{\"text\":\"大にかける\",\"color\":\"green\",\"bold\":true,\"hoverEvent\":{\"action\":\"show_text\",\"value\":[{\"text\":\"小(サイコロの和 8~12)にかけます\\n当選すると掛け金の2倍の額をゲット！\",\"color\":\"aqua\"}]}," +
+                "\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/chn high\"}},{\"text\":\"＞\",\"color\":\"white\",\"hoverEvent\":{\"action\":\"show_text\",\"value\":[{\"text\":\"\",\"color\":\"aqua\"}]}}]";
+
+        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "tellraw @a " + json);
     }
 
     public boolean getMoney(Player player, int needMoney) {
@@ -105,7 +114,10 @@ public class GameManager {
                 .replace("{time}", String.valueOf(startCount - 15));
 
         player.sendMessage(message);
-    }
+        for (Player allPlayer : Bukkit.getServer().getOnlinePlayers()) {
+            allPlayer.playSound(player, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
+        }
+        }
 
     public void highJoin(Player player){
         if (!gameActive) {
@@ -148,6 +160,9 @@ public class GameManager {
     public void startCount() {
         Bukkit.getScheduler().runTaskLater(PetoCasinoV2.getInstance(), () -> {
             startCount --;
+            if (startCount == 45 || startCount == 30 || startCount == 20 || startCount == 18 || startCount == 17 || startCount == 16){
+                countDownChat(startCount - 15);
+            }
             if (startCount == 15){
                 prepareRoll();
             } else if (startCount == 10){
@@ -161,6 +176,17 @@ public class GameManager {
                 startCount();
             }
         }, 20L);
+    }
+
+    public void countDownChat(int sec) {
+        for (Player player : participants) {
+            String message = plugin.getConfig().getString("messages.game_time");
+            message = message.replace("{time}", String.valueOf(sec));
+            player.sendMessage(message);
+            if (sec <= 3) {
+                player.playSound(player, Sound.UI_BUTTON_CLICK, 1.0f, 1.0f);
+            }
+        }
     }
 
     public void cancelCount() {
@@ -180,6 +206,7 @@ public class GameManager {
 
     public void endGame() {
         int sumDice = dice1 + dice2;
+        winners.clear();
         String result = "";
 
         if (sumDice <= 6){
@@ -200,36 +227,64 @@ public class GameManager {
 
             if (result.equals("low") && userSelected.equals("low")){
                 String gameStartedMessage = plugin.getConfig().getString("messages.winner_paid");
-                gameStartedMessage = gameStartedMessage.replace("{player}", player.getName())
-                        .replace("{amount}", String.valueOf(betAmount));
+                gameStartedMessage = gameStartedMessage.replace("{select}", userSelected)
+                        .replace("{amount}", String.valueOf(betAmount * 2));
+
                 player.sendMessage(gameStartedMessage);
+
+                player.playSound(player, Sound.UI_TOAST_CHALLENGE_COMPLETE, 0.6f, 1.4f);
+
+                addWinner(player.getName());
 
                 Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "money give " + player.getName() + " " + betAmount * 2);
             }
              else if (result.equals("lucky7") && userSelected.equals("lucky7")){
                 String gameStartedMessage = plugin.getConfig().getString("messages.winner_paid");
-                gameStartedMessage = gameStartedMessage.replace("{player}", player.getName())
-                        .replace("{amount}", String.valueOf(betAmount));
+                gameStartedMessage = gameStartedMessage.replace("{select}",userSelected)
+                        .replace("{amount}", String.valueOf(betAmount * 3));
                 player.sendMessage(gameStartedMessage);
+
+                player.playSound(player, Sound.UI_TOAST_CHALLENGE_COMPLETE, 0.6f, 1.4f);
+
+                addWinner(player.getName());
 
                 Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "money give " + player.getName() + " " + betAmount * 3);
             }
              else if (result.equals("high") && userSelected.equals("high")){
                 String gameStartedMessage = plugin.getConfig().getString("messages.winner_paid");
-                gameStartedMessage = gameStartedMessage.replace("{player}", player.getName())
-                        .replace("{amount}", String.valueOf(betAmount));
+                gameStartedMessage = gameStartedMessage.replace("{select}", userSelected)
+                        .replace("{amount}", String.valueOf(betAmount * 2));
                 player.sendMessage(gameStartedMessage);
+
+                player.playSound(player, Sound.UI_TOAST_CHALLENGE_COMPLETE, 0.6f, 1.4f);
+
+                addWinner(player.getName());
 
                 Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "money give " + player.getName() + " " + betAmount * 2);
             } else {
                 String gameStartedMessage = plugin.getConfig().getString("messages.loser_message");
-                gameStartedMessage = gameStartedMessage.replace("{player}", player.getName());
+                gameStartedMessage = gameStartedMessage.replace("{select}", userSelected);
                 player.sendMessage(gameStartedMessage);
+                player.playSound(player, Sound.BLOCK_NOTE_BLOCK_BASS, 1f,1f);
             }
+        }
+
+        String gameStartedMessage = plugin.getConfig().getString("messages.winner_result");
+        gameStartedMessage = gameStartedMessage.replace("{winners}", getWinnersAsString());
+        for (Player allPlayer : Bukkit.getServer().getOnlinePlayers()) {
+            allPlayer.sendMessage(gameStartedMessage);
         }
         this.gameActive = false;
         participants.clear();
         userSelect.clear();
+    }
+
+    public void addWinner(String playerName) {
+        winners.add(playerName);
+    }
+
+    public String getWinnersAsString() {
+        return String.join(",", winners);
     }
 
 
@@ -239,6 +294,7 @@ public class GameManager {
             String message = plugin.getConfig().getString("messages.draw_dice1");
             message = message.replace("{number}", String.valueOf(dice1));
             player.sendMessage(message);
+            player.playSound(player, Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 0.8f);
         }
     }
     public void dice2() {
@@ -247,6 +303,7 @@ public class GameManager {
             String message = plugin.getConfig().getString("messages.draw_dice2");
             message = message.replace("{number}", String.valueOf(dice2));
             player.sendMessage(message);
+            player.playSound(player, Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 0.8f);
         }
     }
 }
